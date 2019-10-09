5b677a619243955832ca62cb51c459c33284c056
ThalesMachado <thalesmid.uff.br>
2019-10-09 17:04:00 -0300

    AB#208 início da modelagem do usuário

--- schemas/Usuario.schema.js
+++ schemas/Usuario.schema.js
@@ -25,7 +25,7 @@ module.exports({
                     console.error(err);
                     
                 }
-                return resp;
+                return resp; //TODO: testar função
             }
         )
     }
