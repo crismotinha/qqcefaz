4b74a6b36a9e5946d21dc29efbfec57f7d635de8
ThalesMachado <thalesmid.uff.br>
2019-10-09 17:03:11 -0300

    AB#208 Início da modelagem do schema do usuario

---
+++ schemas/Usuario.schema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const database = require('../services/database.services');
+
+const Schema = mongoose.Schema;
+const Model = mongoose.Model();
+const connection = database.dbConnect;
+
+const Usuario = Model('Usuario', new Schema({
+    nome: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    senha: String,
+    token: String,
+    usuario: String
+}));
+
+module.exports({
+    Usuario: Usuario,
+    cadastrarUsuario: (novoUsuario, upsert = false) => {
+        novoUsuario.save().then(
+            err, resp => {
+                if (err) {
+                    console.error(err);
+                    
+                }
+                return resp;
+            }
+        )
+    }
+})
