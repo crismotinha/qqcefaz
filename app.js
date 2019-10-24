cf9320cc56cbc519347398689639ba02429c02e5
Leticia Vidal <leticia.vidal@visagio.com>
2019-10-24 18:38:01 -0300

    #225 alguns ajustes no front: cores, fontes e agora tem o sidenav no desktop

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+var hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
